c9f3f3a545e40285cb3ca4006e658a615071fe5b
Vincent Partington <vpartington@xebialabs.com>
2018-10-17 14:34:22 +0200

    Adds the proper fix to run xebialabsunsupported/ecommerce-monolith:latest on Fargate.

--- xebialabs/fargate-image/fix-hostname.sh
+++ xebialabs/fargate-image/fix-hostname.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-echo "$(hostname -i | cut -d\  -f 1) $(hostname)" >> /etc/hosts
+myip="$(/sbin/ifconfig eth0 | grep 'inet addr' | cut -d: -f2 | awk '{print $1}')"
+myhostname="$(hostname)"
+echo "$myip $myhostname" >> /etc/hosts
 su jhipster ./entrypoint.sh
